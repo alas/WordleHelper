d769d0c4ee2df4875d024fed973ac4eca55deb74
Alas <alastors@gmail.com>
2022-04-05 05:00:26 -0300

    .

--- WordleHelper/Pages/Index.razor.cs
+++ WordleHelper/Pages/Index.razor.cs
@@ -3,7 +3,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using System;
-using System.Globalization;
 using System.Text.RegularExpressions;
 
 public partial class Index
