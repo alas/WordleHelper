d2c05d778504e2b8a6315cc155c00bd76cf5c067
Alas <alastors@gmail.com>
2022-04-06 04:38:51 -0300

    .

--- WordleHelper/wwwroot/service-worker.js
+++ WordleHelper/wwwroot/service-worker.js
@@ -2,24 +2,4 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(cacheName).then((cache) => cache.addAll(
-            [
-                "/css/bootstrap/bootstrap.min.css",
-                "/css/app.css",
-                "/WordleHelper.styles.css"
-            ]
-        ))
-    );
-});
-
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.open("mysite-dynamic").then((cache) => fetch(event.request)
-            .then((response) => {
-                cache.put(event.request, response.clone());
-                return response;
-            }))
-    );
-});
+self.addEventListener("fetch", () => {});
