fe6c32e46b2489395c655dfba54d7972900103d3
Alas <alastors@gmail.com>
2022-04-09 00:46:11 -0300

    .

--- WordListsGenerator/Compress.cs
+++ WordListsGenerator/Compress.cs
@@ -14,22 +14,10 @@ internal static class Compress
 
         await input.CopyToAsync(stream);
         await stream.FlushAsync();
+        await output.FlushAsync();
 
         var result = output.ToArray();
 
         return Convert.ToBase64String(result);
-    }
-
-    public static async Task<string> FromGzipAsync(this string value)
-    {
-        var bytes = Convert.FromBase64String(value);
-        await using var input = new MemoryStream(bytes);
-        await using var output = new MemoryStream();
-        await using var stream = new GZipStream(input, CompressionMode.Decompress);
-
-        await stream.CopyToAsync(output);
-        await stream.FlushAsync();
-
-        return Encoding.Unicode.GetString(output.ToArray());
     }
 }
--- WordListsGenerator/Program.cs
+++ WordListsGenerator/Program.cs
@@ -1,10 +1,8 @@
-﻿using System.IO.Compression;
-using WordListsGenerator;
+﻿using WordListsGenerator;
 
 var english = WordList.GetAllWordsEnglish();
 var spanish = WordList.GetAllWordsSpanish();
 
-
-//this generates the smallest files
-File.WriteAllText(@"english gzip Optimal.txt", Compress.ToGzipAsync(english).Result);
-File.WriteAllText(@"spanish gzip Optimal.txt", Compress.ToGzipAsync(spanish).Result);
+//this generates the smallest files not counting brotli that is not supported on platform: browser even when js blazor is loaded using brotli...
+File.WriteAllText(@"english gzip Optimal.txt", english.ToGzipAsync().Result);
+File.WriteAllText(@"spanish gzip Optimal.txt", spanish.ToGzipAsync().Result);
